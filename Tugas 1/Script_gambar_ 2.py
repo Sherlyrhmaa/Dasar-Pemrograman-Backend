c89756213a7e83f7dd1d12b6dfd30168dcff5d44
Sherlyrhmaa <168067974+Sherlyrhmaa@users.noreply.github.com>
2024-09-27 09:41:48 +0700

    Add files via upload

---
+++ Tugas 1/Script_gambar_ 2.py
@@ -0,0 +1,8 @@
+password_benar = "112233"
+password = input("Masukkan password: ")
+
+if password == password_benar:
+    print("Selamat datang bos!")
+else:
+    print("Password salah, coba lagi!")
+    print("Terimakasih sudah menggunakan aplikasi kami.")
---
+++ Tugas 1/Script_gambar_1.py
@@ -0,0 +1,7 @@
+total_belanja = float(input("Masukkan total belanja: "))
+
+if total_belanja > 100000:
+    print("Selamat, Anda dapat hadiah!")
+    print("Terimakasih sudah berbelanja")
+else:
+    print("Terimakasih sudah berbelanja.")
