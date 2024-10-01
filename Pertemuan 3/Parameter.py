3403d3bc1632685318463b6b863ee1010f01d84c
Sherlyrhmaa <168067974+Sherlyrhmaa@users.noreply.github.com>
2024-10-01 09:38:29 +0700

    Add files via upload

---
+++ Pertemuan 3/Parameter.py
@@ -0,0 +1,20 @@
+#Contoh 1
+def sapa_teman(nama):
+    print("Hai", nama);
+
+sapa_teman("Lisa")
+
+#Contoh 2
+def sapa_teman(nama1, nama2, nama3):
+    print("Hai", nama1, nam2, nam3);
+
+sapa_teman("Lisa", "Nova", "Putri")
+
+#Contoh 3
+def hitung_luas_segitiga(alas, tinggi):
+    luas = (alas * tinggi)/ 2
+    print("Luas segitiga adalah: ",luas)
+
+hitung_luas_segitiga(5, 7)
+hitung_luas_segitiga(2, 10)
+hitung_luas_segitiga(191, 357)
---
+++ Pertemuan 3/Return_dalam_fungsi.py
@@ -0,0 +1,6 @@
+def harga_setelah_pajak(harga_dasar):
+    return harga_dasar + (harga_dasar * 10/100)
+
+harga_cilok = 5000
+harga_final_cilok = harga_setelah_pajak(harga_cilok)
+print("Harga cilok 1 porsi Rp.", harga_final_cilok)
